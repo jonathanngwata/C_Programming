d0bed75ac68b40962c0ca61a28e1134f76a76fcb
jonathanngwata <ngwatajonathan@gmail.com>
2024-06-23 03:18:54 +0200

    do while demo

---
+++ C_Loops/do_while/do_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int z =0;
+    do
+    {
+       
+        printf("Git bash");
+        printf("\n");
+        printf("Commands");
+        printf("\n");
+        z++;
+        printf("Git hub\n");
+        
+    }while(z < 3);
+    
+    return 0;
+}
